6635563da92d3ed060124caba33ff3413bd4dc32
rizal476 <38076489+rizal476@users.noreply.github.com>
2019-07-14 11:08:32 +0800

    1

--- application/views/admin_page.php
+++ application/views/admin_page.php
@@ -47,6 +47,15 @@
 
     <div class="site-wrap">
 
+        <div class="site-mobile-menu site-navbar-target">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+            </div>
+            <div class="site-mobile-menu-body"></div>
+        </div>
+
         <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
 
         <div class="container" >
@@ -88,7 +97,13 @@
                     </li>
                 </ul>                                                                                                                                                                                                                                                                                          </ul>
                 </nav>
+            </div>
+            <div class="ml-auto">
+                <div class="social-wrap">
 
+                <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
+                    class="icon-menu h3"></span></a>
+                </div>
             </div>
             </div>
         </div>
